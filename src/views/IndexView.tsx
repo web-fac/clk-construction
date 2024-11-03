e9aae1bedb7b40216e61147982c5093b437775e5
Jacob Miller <dev@jacobmiller22.com>
2024-11-03 12:49:38 -0500

    chore: removed unused code

--- src/views/IndexView.tsx
+++ src/views/IndexView.tsx
@@ -78,36 +78,6 @@ const IndexView = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="fg-container flex items-center mx-auto">
-        <div className="flex flex-col items-center space-y-4 text-center">
-          <div className="space-y-2">
-            <Typography
-              element="h1"
-              as="h1"
-              className="text-primary-foreground"
-            >
-              Building Your Dreams, One Project at a Time
-            </Typography>
-            <Typography
-              element="p"
-              as="p"
-              className="mx-auto max-w-[700px] text-primary-foreground"
-            >
-              CLK Construction: Your trusted partner for residential and light
-              commercial construction, remodeling, and custom decks.
-            </Typography>
-          </div>
-          <div className="space-x-4">
-            <Button variant="outline" className="text-primary">
-              Get a Quote
-            </Button>
-            <Button variant="outline" className="text-primary">
-              Our Services
-            </Button>
-          </div>
-        </div>
-      </div> */}
     </ViewLayout>
   );
 };
